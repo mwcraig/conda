e79fc8864d6a89ba37907c17cf068bd9924bc2c0
Ivan Smirnov <i.s.smirnov@gmail.com>
2014-01-31 18:25:55 +0000

    Use square bracket syntax

--- bin/conda.fish
+++ bin/conda.fish
@@ -33,7 +33,7 @@
 
 # Require version fish v2.0+ to be able to use array slices, `else if`
 # and $status for command substitutions
-if test (echo (fish -v ^&1) | sed 's/^.*version \([0-9]\)\..*$/\1/') -lt 2
+if [ (echo (fish -v ^&1) | sed 's/^.*version \([0-9]\)\..*$/\1/') -lt 2 ]
     echo "Incompatible fish shell version; please upgrade to v2.0 or higher."
     exit 1
 end
@@ -42,10 +42,10 @@ end
 # Calls activate / deactivate functions if the first argument is activate or
 # deactivate; otherwise, calls conda-<cmd> and passes the arguments through
 function conda
-    if test (count $argv) -lt 1
+    if [ (count $argv) -lt 1 ]
         command conda
     else
-        if test (count $argv) -gt 1
+        if [ (count $argv) -gt 1 ]
             set ARGS $argv[2..-1]
         else
             set -e ARGS
@@ -72,11 +72,11 @@ function activate --description "Activate a conda environment."
     # deactivate an environment first if it's set
     if set -q CONDA_DEFAULT_ENV
         conda ..checkenv $argv[1]
-        if test $status = 0
+        if [ $status = 0 ]
             set -l NEW_PATH (conda "..deactivate")
             # convert colon-separated path to a fish list
             set -gx PATH (echo $NEW_PATH | sed 's/:/\n/g')
-            if test (conda ..changeps1)
+            if [ (conda ..changeps1) ]
                 set -gx PS1 $CONDA_OLD_PS1
                 set -e CONDA_OLD_PS1
             end
@@ -87,17 +87,17 @@ function activate --description "Activate a conda environment."
 
     # try to activate the environment
     set -l NEW_PATH (conda ..activate "$argv[1]")
-    if test $status = 0
+    if [ $status = 0 ]
         # convert colon-separated path to a fish list
         set -gx PATH (echo $NEW_PATH | sed 's/:/\n/g')
-        if test (echo "$argv[1]" | grep "/")
+        if [ (echo "$argv[1]" | grep "/") ]
             pushd (dirname $argv[1])
             set -gx CONDA_DEFAULT_ENV (pwd)/(basename $argv[1])
             popd
         else
             set -gx CONDA_DEFAULT_ENV $argv[1]
         end
-        if test (conda ..changeps1)
+        if [ (conda ..changeps1) ]
             set -gx CONDA_OLD_PS1 $PS1
             set -gx PS1 "($CONDA_DEFAULT_ENV)$PS1"
         end
@@ -110,11 +110,11 @@ end
 # Equivalent to bash version of conda deactivate script
 function deactivate --description "Deactivate the current conda environment."
     set -l NEW_PATH (conda ..deactivate $argv[1])
-    if test $status = 0
+    if [ $status = 0 ]
         # convert colon-separated path to a fish list
         set -gx PATH (echo $NEW_PATH | sed 's/:/\n/g')
         set -e CONDA_DEFAULT_ENV
-        if test (conda ..changeps1)
+        if [ (conda ..changeps1) ]
             set -gx PS1 $CONDA_OLD_PS1
             set -e CONDA_OLD_PS1
         end
